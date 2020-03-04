2374bd8eb3d58e536a77ab5a43d1e54ff023f5c2
aatuolk <Aatu.olkkonen@student.lut.fi>
2020-03-04 13:36:43 +0200

    toka

--- app/src/main/java/com/example/moovi/MainActivity.java
+++ app/src/main/java/com/example/moovi/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        jer on homo e
         setContentView(R.layout.activity_main);
     }
 }
