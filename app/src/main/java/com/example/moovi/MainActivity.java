5680e7fa55c7a80451590e28436f169229c1e9ff
aatuolk <Aatu.olkkonen@student.lut.fi>
2020-03-04 13:41:06 +0200

    Korjattu testaus

--- app/src/main/java/com/example/moovi/MainActivity.java
+++ app/src/main/java/com/example/moovi/MainActivity.java
@@ -9,7 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        jer on homo e
         setContentView(R.layout.activity_main);
     }
 }
