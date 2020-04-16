2c228b94055a7fcbcb0a90cc365cc20f320e2095
jerekuo <jere.kuolimo@student.lut.fi>
2020-04-16 11:07:54 +0300

    commit testi

--- app/src/main/java/com/example/moovi/MainActivity.java
+++ app/src/main/java/com/example/moovi/MainActivity.java
@@ -3,13 +3,22 @@ package com.example.moovi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        jer on homo e
         setContentView(R.layout.activity_main);
+    }
+
+    //takes user to register window
+    public void toRegister(View v) {
+
+    }
+    //takes user to login window
+    public void toLogin(View v) {
+
     }
 }
