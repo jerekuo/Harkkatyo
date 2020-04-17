fbd73da118fccf105a9421116c29dfa73300b387
Atsolind <atso.lindstrom@student.lut.fi>
2020-04-17 15:06:27 +0300

    calendar fragment

---
+++ app/src/main/java/com/example/moovi/Calendar_Fragment.java
@@ -0,0 +1,34 @@
+package com.example.moovi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Calendar_Fragment extends Fragment {
+    View view;
+
+    public Calendar_Fragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_calendar_, container, false);
+        CalendarView calendarView = view.findViewById(R.id.calendarView);
+        
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar_, container, false);
+
+
+    }
+}
