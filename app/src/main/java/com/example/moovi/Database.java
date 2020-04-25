479d4619d25d7612db8e5db6c4fb133763a82f9c
aatuolk <Aatu.olkkonen@student.lut.fi>
2020-04-25 12:26:03 +0300

    Hallsit ja roomsit tulee firestoresta

--- app/src/main/java/com/example/moovi/Database.java
+++ app/src/main/java/com/example/moovi/Database.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,12 +13,31 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
+
+
 
 public class Database {
     HallSystem hallsystem = HallSystem.getInstance();
     private static Database instance = new Database();
     FirebaseUser user = HallSystem.getInstance().getUser();
+
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    ArrayList<Hall> halls;
+    ArrayList<Room> rooms;
 
 
 
@@ -66,4 +86,53 @@ public class Database {
 
 
     }
+    public void writeHallList(){
+        halls = new ArrayList<>();
+        db.collection("AllHalls")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        int i=0;
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                String name = document.getData().get("name").toString();
+                                String address = document.getData().get("address").toString();
+                                halls.add(new Hall(i,name,address));
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                i++;
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+        //Setting halls to hallList
+        hallsystem.setHallList(halls);
+    }
+
+    public void writeRoomList(Hall h){
+        String hall = h.getHallName();
+        db.collection("AllHalls").document(hall).collection("Rooms").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                int i = 0;
+                rooms = new ArrayList<>();
+
+                for (QueryDocumentSnapshot documentsnapshot : queryDocumentSnapshots){
+                    i++;
+                    String name = documentsnapshot.getData().get("name").toString();
+                    int cap = Integer.parseInt(documentsnapshot.getData().get("capacity").toString());
+                    System.out.println(name);
+                    System.out.println(cap);
+                    System.out.println(i);
+                    rooms.add(new Room(name,cap,"Kaikki pelaa",i));
+                    Log.d(TAG, documentsnapshot.getId() + " => " + documentsnapshot.getData());
+                } hallsystem.setRoomList(rooms);
+            }
+        });
+    }
+
+
 }
